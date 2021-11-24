381e9ea966eaf32a43a485a3e719a6e403593e58
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-24 13:49:58 +0100

    add API calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-undef */
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
   const [geolocation, setGeoLocation] = useState({});
@@ -11,7 +12,15 @@ const App = () => {
 
       const latitude = geolocation.latitude;
       const longitude = geolocation.longitude;
-    });
+
+      let locationResponse = await axios.get(
+        `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key={placeholder}`
+      );
+
+      let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid={placeholder}`
+      );
+        debugger
+    )
   }, []);
 
   return (
