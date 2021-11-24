520bd9814b3f9015e6be5a521816d9c9f3d96e7f
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-24 18:00:40 +0100

    hide API keys

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,33 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-
   const [geolocation, setGeoLocation] = useState({});
-  
-  
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async (position) => {
-      
-      setGeoLocation(position.coords)
-      
-      const latitude = position.latitude
-      const longitude =  position.longitude
+      setGeoLocation(position.coords);
 
-        let locationResponse = await axios.get(
-          `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key={placeholder}`
-        );
+      const latitude = position.latitude;
+      const longitude = position.longitude;
 
-        let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid={placeholder}`
-        );
+      let locationResponse = await axios.get(
+        `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=process.env.REACT_APP_OPEN_CAGE_API_KEY`
+      );
 
-        let weatherInfo = {
-          city: locationResponse.data.results[0].components.postal_city,
-          temp: weatherResponse.data.current.temp
-        }
+      let weatherResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=process.env.REACT_APP_OPEN_WEATHER_API_KEY`
+      );
 
-      
-        
-      })
+      let weatherInfo = {
+        city: locationResponse.data.results[0].components.postal_city,
+        temp: weatherResponse.data.current.temp,
+      };
+    });
 
-        debugger
-    
-    []}
-    
-    )
+    debugger;
+
+    [];
+  });
 
   return (
     <div data-cy="weather-display">
@@ -44,6 +38,6 @@ const App = () => {
       <div data-cy="location">Virum</div>
     </div>
   );
-}
+};
 
 export default App;
