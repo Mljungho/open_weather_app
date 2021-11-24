08d7d01d109dfac9e1f875cce1cf48455ea172df
Mljungho <mathias.ljungholm@gmail.com>
2021-11-24 09:55:04 +0100

    install axios, add use effect for geolocation. need to be tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const app = () => {
-  navigator.geolocation.getCurrentPosition(async (position) => {});
+const App = () => {
+  const [geolocation, setGeolocation] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      setGeolLocation({ geolocation: position.coords });
+    });
+  }, []);
+
   return (
     <div data-cy="weather-display">
       <div data-cy="temp">17</div>
@@ -10,4 +17,4 @@ const app = () => {
   );
 };
 
-export default app;
+export default App;
